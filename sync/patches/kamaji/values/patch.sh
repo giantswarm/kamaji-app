482a25003c605eb7c771ff00cfc1011cf68fd589
Simon Weald <glitchcrab-github@simonweald.com>
2026-01-19 21:29:07 +0000

    add values schema to kamaji chart

--- sync/patches/kamaji/values/patch.sh
+++ sync/patches/kamaji/values/patch.sh
@@ -6,12 +6,16 @@ set -o pipefail
 
 repo_dir=$(git rev-parse --show-toplevel) ; readonly repo_dir
 script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ) ; readonly script_dir
+CHART_DIR="${repo_dir}/helm/kamaji" ; readonly CHART_DIR
 
 cd "${repo_dir}"
 
 readonly script_dir_rel=".${script_dir#"${repo_dir}"}"
 
 set -x
+
 git apply "${script_dir_rel}/_values.patch"
+
+cp -av "${script_dir_rel}/manifests/values.schema.json" "${CHART_DIR}/values.schema.json"
 
 { set +x; } 2>/dev/null
